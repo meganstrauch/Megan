b21eec208904b04cb8435cfe56bb392f5ce93393
macbook <macbook@macbooks-Air.hsd1.fl.comcast.net>
2020-04-12 19:11:48 -0400

    jQuery fixed

--- homework/jQuery/js/jQuery.js
+++ homework/jQuery/js/jQuery.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
   $("#set2").hide();
   $("#set3").hide();
   $("#set4").hide();
+  $("#set5").hide();
 
   $("#button1").click(function(){
     $("#set1").show();
     $("#set2").hide();
     $("#set3").hide();
     $("#set4").hide();
+    $("#set5").hide();
 });
 
 $("#button2").click(function(){
@@ -16,6 +18,7 @@ $("#button2").click(function(){
   $("#set2").show();
   $("#set3").hide();
   $("#set4").hide();
+  $("#set5").hide();
 });
 
 $("#button3").click(function(){
@@ -23,6 +26,7 @@ $("#button3").click(function(){
   $("#set2").hide();
   $("#set3").show();
   $("#set4").hide();
+  $("#set5").hide();
 });
 
 $("#button4").click(function(){
@@ -30,6 +34,40 @@ $("#button4").click(function(){
   $("#set2").hide();
   $("#set3").hide();
   $("#set4").show();
+  $("#set5").hide();
 });
+
+$("#button5").click(function(){
+  $("#set1").hide();
+  $("#set2").hide();
+  $("#set3").hide();
+  $("#set4").hide();
+  $("#set5").show();
+});
+
+$("#button4").click(function(){
+  $(".main div:nth-child(4) p").css("color", "pink");
+});
+
+$("#button2").click(function() {
+  $(".body h2").html("Conch");
+});
+
+$("#button1").click(function() {
+  $(".body h2").html("Exuma Islands ");
+});
+
+$("#button3").click(function() {
+  $(".body h2").html("Pigs Beach ");
+});
+
+$("#button4").click(function() {
+  $(".body h2").html("Thunderball Cave ");
+});
+
+$("#button5").click(function() {
+  $(".body h2").html("Swimming With Sharks ");
+});
+
 
 });
