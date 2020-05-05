e0541d58b29bc6c3fe8ca48420a2197ca60e4d6e
macbook <macbook@macbooks-MacBook-Air.local>
2020-05-04 20:30:33 -0400

    final project

---
+++ homework/contact/js/contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+
+  $("#set1").hide();
+  $("#set2").hide();
+
+
+  $("#button1").click(function(){
+    $("#set1").show();
+    $("#set2").hide();
+  });
+
+  $("#button2").click(function(){
+  $("#set1").hide();
+  $("#set2").show();
+  
+  });
+
+
+
+
+
+});
